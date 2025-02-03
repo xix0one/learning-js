819227cb9200f486c4ef109bbf7d16b4927f3848
xix0one <petrovalex2014@ya.ru>
2025-02-03 14:52:49 +0800

    fix

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -16,20 +16,65 @@ let imagesModal = document.getElementById("imagesModal");
 
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
-let amount = document.getElementById("amount");
 let addToCart = document.getElementById("addToCart");
 
-function closeBasket() {
-    windowBasket.className = "divBasket";
-    document.body.removeEventListener("mouseup", closeBasket);
+let closeModal = document.getElementById("closeModal");
+
+let iconPreviousTelephone = document.getElementById("iconPreviousTelephone");
+let iconNextTelephone = document.getElementById("iconNextTelephone");
+
+// shop
+let amount = document.getElementById("amount");
+let p = listBasket.getElementsByTagName("p")[0];
+let shopOpen = document.getElementById("shopOpen");
+let defaultDivBasket = listBasket.innerHTML;
+let checkout = document.getElementById("checkout");
+let deleteShop = document.getElementById("deleteShop");
+let countShop = document.getElementById("countShop");
+let totalShop = document.getElementById("totalShop");
+
+function closeBasket(event) {
+    if (event.target.id !== "windowBasket") {
+        windowBasket.className = "divBasket";
+        document.body.removeEventListener("click", closeBasket);
+        countSneakers.removeEventListener("click", closeBasket);
+        basket.removeEventListener("click", closeBasket);
+        avatar.removeEventListener("click", closeBasket);
+    }
 }
 
 function addOpenBasket(elem) {
-    elem.addEventListener("mousedown", function() {
-        windowBasket.className += " d-flex flex-column mt-5 shadow p-3 bg-body-tertiary rounded";
-        document.body.addEventListener("mouseup", closeBasket);
+    elem.addEventListener("click", function(event) {
+        event.stopPropagation();
+        windowBasket.className = "divBasket d-flex flex-column mt-5 shadow p-3 bg-body-tertiary rounded z-3";
+        document.body.addEventListener("click", closeBasket);
+        countSneakers.addEventListener("click", closeBasket);
+        basket.addEventListener("click", closeBasket);
+        avatar.addEventListener("click", closeBasket);
     });
 }
+
+function readDivImages() {
+    let current = 0;
+    for (let i = 0; i < imagesModal.children.length; ++i) {
+        if (imagesModal.children[i].className == "chooseImage") {
+            imagesModal.children[i].className = "";
+            current = i;
+        }
+    }
+    return current;
+}
+
+closeModal.addEventListener("mousedown", function() {
+    for (let i = 0; i < imagesDiv.children.length; ++i) {
+        if (imagesDiv.children[i].className == "chooseImage") 
+            imagesModal.children[i].className = "chooseImage";
+        else
+            imagesModal.children[i].className = "";
+    }
+    mainImageModal.src = mainImageDiv.src;
+});
+
 
 function imagesOpen(elem, mainImage) {
     for (let i = 0; i < elem.children.length; ++i) {
@@ -71,17 +116,6 @@ function nextButtonDiv(current) {
     }
 }
 
-function readDivImages() {
-    let current = 0;
-    for (let i = 0; i < imagesModal.children.length; ++i) {
-        if (imagesModal.children[i].className == "chooseImage") {
-            imagesModal.children[i].className = "";
-            current = i;
-        }
-    }
-    return current;
-}
-
 function previousNextButtons() {
     previousButton.addEventListener("mousedown", function(event) {
         event.preventDefault();
@@ -104,7 +138,40 @@ function minusAmount() {
 minus.addEventListener("mousedown", minusAmount);
 
 addToCart.addEventListener("mousedown", function() {
-    
+    document.body.addEventListener("click", closeBasket);
+    if (Number(amount.textContent) > 0) {
+        p.remove();
+        shopOpen.className = "shop d-flex justify-content-betweenalign-items-center";
+        windowBasket.style.height = "auto";
+        checkout.style.display = "block";
+        countShop.textContent = amount.textContent;
+        totalShop.textContent = "$" + (125 * Number(countShop.textContent)).toFixed(2);
+        countSneakers.textContent = amount.textContent;
+        countSneakers.className = "d-inline-flex justify-content-center";
+    }
+});
+
+deleteShop.addEventListener("mousedown", function() {
+    shopOpen.className = "shop";
+    countShop.textContent = 0;
+    totalShop.textContent = 0;
+    amount.textContent = 0;
+    countSneakers.className = "";
+    checkout.style.display = "none";
+    listBasket.append(p);
+});
+
+let currentIconTelephone = 1;
+iconPreviousTelephone.addEventListener("mousedown", function() {
+    if (currentIconTelephone == 1) currentIconTelephone = 4;
+    else --currentIconTelephone;
+    mainImageDiv.src = "images/image-product-" + currentIconTelephone + ".jpg"
+});
+
+iconNextTelephone.addEventListener("mousedown", function() {
+    if (currentIconTelephone == 4) currentIconTelephone = 1;
+    else ++currentIconTelephone;
+    mainImageDiv.src = "images/image-product-" + currentIconTelephone + ".jpg"
 });
 
 addOpenBasket(countSneakers); 
