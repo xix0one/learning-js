502464c461ff4b5c756d4eee08d378b504650b2a
xix0one <petrovalex2014@ya.ru>
2025-02-18 08:18:33 +0800

    added changes

--- todo-app-main/main.js
+++ todo-app-main/main.js
@@ -2,8 +2,20 @@
 
 let todoDiv = document.getElementById("todo");
 let count = document.getElementById("count");
+let theme = document.getElementById("theme");
 
 let list = { "javascript course": false, "jog around park": false, "read": false, "pick up groceries": false };
+
+function delDiv(event) {
+    let el = event.target.className;
+    let reg = new RegExp(el);
+    delete list[el];
+    addCount();
+
+    for (let i = 0; i < todoDiv.childNodes.length; ++i) {
+        if (reg.test(todoDiv.childNodes[i].className)) todoDiv.childNodes[i].remove();
+    }
+}
 
 function addCount() {
     if (Object.keys(list).length > 0) {
@@ -14,7 +26,7 @@ function addCount() {
 function addItemInDiv() {
     for (let key in list) {
         let div = document.createElement("div");
-        div.className = "d-flex flex-row align-items-center";
+        div.className = "d-flex flex-row align-items-center " + key;
 
         let span = document.createElement("span");
         span.textContent = key;
@@ -26,9 +38,12 @@ function addItemInDiv() {
 
         let spanIMG = document.createElement("span");
         spanIMG.id = "spanIMG";
+        spanIMG.className = key;
+        spanIMG.addEventListener("mousedown", delDiv);
         let img = document.createElement("img");
         img.src = "images/icon-cross.svg";
         img.alt = "icon-cross";
+        img.className = key;
         spanIMG.append(img);
 
         div.append(checkBox);
