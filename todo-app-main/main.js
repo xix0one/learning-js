03b2f3356b4d4cf1e2da78ac9287fcc1e6c0a044
xix0one <petrovalex2014@ya.ru>
2025-02-26 10:53:35 +0800

    changes

--- todo-app-main/main.js
+++ todo-app-main/main.js
@@ -3,7 +3,11 @@
 let todoDiv = document.getElementById("todo");
 let count = document.getElementById("count");
 let theme = document.getElementById("theme");
+let iconTheme = document.getElementById("imgTheme");
+let main = document.body.getElementsByTagName("main")[0];
+let inputText = document.getElementById("inputText");
 
+let darkTheme = true;
 let list = { "javascript course": false, "jog around park": false, "read": false, "pick up groceries": false };
 
 function delDiv(event) {
@@ -13,7 +17,8 @@ function delDiv(event) {
     addCount();
 
     for (let i = 0; i < todoDiv.childNodes.length; ++i) {
-        if (reg.test(todoDiv.childNodes[i].className)) todoDiv.childNodes[i].remove();
+        if (reg.test(todoDiv.childNodes[i].className)) 
+            todoDiv.childNodes[i].remove();
     }
 }
 
@@ -23,6 +28,52 @@ function addCount() {
     } else count.textContent = "";
 }
 
+function checkCheckBox(event) {
+    if (!list[event.target.className]) {
+        list[event.target.className] = true;
+        event.target.nextSibling.style.textDecoration = "line-through";
+        event.target.nextSibling.style.color = "hsl(234, 11%, 52%)";
+    }
+    else {
+        list[event.target.className] = false;
+        event.target.nextSibling.style = "";
+    }
+}
+
+function changeTheme() {
+    let divs = main.getElementsByTagName("div");
+    let span = todoDiv.getElementsByClassName("textSpan");
+    const white = "hsl(0, 0%, 98%)";
+
+    if (darkTheme) {
+        iconTheme.src = "images/icon-moon.svg";
+        document.body.style.backgroundImage = "url(images/bg-desktop-light.jpg)";
+        document.body.style.backgroundColor = white;
+        inputText.style.backgroundColor = white;
+
+        for (let i = 0; i < divs.length; ++i)
+            divs[i].style.backgroundColor = white;   
+
+        for (let i = 0; i < span.length; ++i) 
+            span[i].style.color = "hsl(235, 19%, 35%)";
+    }
+    else {
+        iconTheme.src = "images/icon-sun.svg";
+        document.body.style.backgroundImage = "";
+        document.body.style.backgroundColor = "";
+        inputText.style.backgroundColor = "";
+
+        for (let i = 0; i < divs.length; ++i) 
+            divs[i].style.backgroundColor = "";
+        
+        for (let i = 0; i < span.length; ++i) 
+            span[i].style.color = "";
+    }
+    darkTheme = !darkTheme;
+}
+
+theme.addEventListener("mousedown", changeTheme);
+
 function addItemInDiv() {
     for (let key in list) {
         let div = document.createElement("div");
@@ -30,11 +81,16 @@ function addItemInDiv() {
 
         let span = document.createElement("span");
         span.textContent = key;
+        span.className = "textSpan";
 
-        if (list[key]) {} // underline
+        if (list[key]) {
+            span.style.textDecoration = "line-through";
+        } 
 
         let checkBox = document.createElement("input");
         checkBox.type = "checkbox";
+        checkBox.className = key;
+        checkBox.addEventListener("change", checkCheckBox);
 
         let spanIMG = document.createElement("span");
         spanIMG.id = "spanIMG";
