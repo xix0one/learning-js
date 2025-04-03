9e2d60bbcae0dc4c35d4d534f8047a0a2bb56282
xix0one <petrovalex2014@ya.ru>
2025-04-03 16:13:19 +0800

    fix tetris

--- tetris/main.js
+++ tetris/main.js
@@ -1,20 +1,27 @@
 "use strict";
 
 const BLOCK_SIZE = 30;
+const SPEED = 300;
+let field = [];
+let interval = null;
+let pause = false;
 
 let left = document.getElementById("left");
 let right = document.getElementById("right");
 let rotation = document.getElementById("rotation");
+let down = document.getElementById("down");
+let lose = document.getElementById("lose");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let restart = document.getElementById("restart");
+let scoreSpan = document.getElementById("score");
+let score = 0;
 
 let canvas = document.getElementById("mainWindow");
 let ctx = canvas.getContext("2d");
 
 canvas.width = 10 * BLOCK_SIZE;
 canvas.height = 20 * BLOCK_SIZE;
-
-let field = [];
-
-let interval = null;
 
 for (let i = 0; i < 20; ++i) {
     let row = [];
@@ -28,7 +35,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
   
-
 class Object {
     constructor() {
         this.x = 4;
@@ -67,76 +73,132 @@ class Object {
 let shapes = [];
 
 let I = new Object();
-I.shape = [[1, 1, 1, 1]];
-I.color = "green";
 shapes.push(I);
 
 let J = new Object();
-J.shape = [[1, 1, 1], 
-           [0, 0, 1]];
-J.color = "yellow";
 shapes.push(J);
 
 let T = new Object();
-T.shape = [[1, 1, 1],
-           [0, 1, 0]];
-T.color = "white";
 shapes.push(T);
 
 let L = new Object();
-L.shape = [[1, 1, 1],
-           [1, 0, 0]];
-L.color = "red";
 shapes.push(L);
 
 let S = new Object();
-S.shape = [[0, 1, 1],
-           [1, 1, 0]];
-S.color = "cyan";
 shapes.push(S);
 
 let Z = new Object();
-Z.shape = [[1, 1, 0],
-           [0, 1, 1]];
-Z.color = "orange";
 shapes.push(Z);
 
 let O = new Object();
-O.shape = [[1, 1],
-           [1, 1]];
-O.color = "purple";
 shapes.push(O);
+
+function defauleShapes() {
+    O.shape = [[1, 1], [1, 1]];
+    O.color = "purple";
+    O.x = 4;
+    O.y = 0;
+
+    Z.shape = [[1, 1, 0], [0, 1, 1]];
+    Z.color = "orange";
+    Z.x = 4;
+    Z.y = 0;
+
+    S.shape = [[0, 1, 1], [1, 1, 0]];
+    S.color = "cyan";
+    S.x = 4;
+    S.y = 0;
+
+    L.shape = [[1, 1, 1], [1, 0, 0]];
+    L.color = "red";
+    L.x = 4;
+    L.y = 0;
+
+    T.shape = [[1, 1, 1], [0, 1, 0]];
+    T.color = "white";
+    T.x = 4;
+    T.y = 0;
+
+    J.shape = [[1, 1, 1], [0, 0, 1]];
+    J.color = "yellow";
+    J.x = 4;
+    J.y = 0;
+
+    I.shape = [[1, 1, 1, 1]];
+    I.color = "green";
+    I.x = 4;
+    I.y = 0;
+}
 
 let currentShape = null;
 
-left.addEventListener("click", leftMove);
-right.addEventListener("click", rightMove);
-rotation.addEventListener("click", rotationMove);
+function addEvents() {
+    left.addEventListener("click", leftMove);
+    right.addEventListener("click", rightMove);
+    rotation.addEventListener("click", rotationMove);
+    down.addEventListener("click", speedUp);
+    document.addEventListener("keyup", buttonEvents);
+    stop.addEventListener("click", stopfun);
+}
+
+function removeEvents() {
+    left.removeEventListener("click", leftMove);
+    right.removeEventListener("click", rightMove);
+    rotation.removeEventListener("click", rotationMove);
+    down.removeEventListener("click", speedUp);
+    document.removeEventListener("keyup", buttonEvents);
+}
+
+function collision(newX, newY, shape) {
+    for (let y = 0; y < shape.length; ++y) {
+        for (let x = 0; x < shape[y].length; ++x) {
+            if (shape[y][x]) {
+                if (field[newY + y][newX + x]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function speedUp() {
+    if (currentShape.y + (currentShape.shape.length - 1) < 19 && !collision(currentShape.x, currentShape.y + 1, currentShape.shape))
+        currentShape.y++;
+}
 
 function rotationMove() {
-    let rotated = [];
-    let shapeY = currentShape.shape.length;
-    let shapeX = currentShape.shape[0].length;
+    if (currentShape.y < (20 - currentShape.shape.length)) {
+        let rotated = [];
+        let shapeY = currentShape.shape.length;
+        let shapeX = currentShape.shape[0].length;
 
-    for (let x = 0; x < shapeX; ++x) {
-        let arr = [];
-        for (let y = shapeY - 1; y >= 0; --y) {
-            arr.push(currentShape.shape[y][x]);
+        for (let x = 0; x < shapeX; ++x) {
+            let arr = [];
+            for (let y = shapeY - 1; y >= 0; --y) {
+                arr.push(currentShape.shape[y][x]);
+            }
+            rotated.push(arr);
         }
-        rotated.push(arr);
-    }
 
-    currentShape.shape = rotated;
+        if (!collision(currentShape.x, currentShape.y, rotated))
+            currentShape.shape = rotated;
+
+        if ((currentShape.x + (currentShape.shape[0].length - 1)) > 9) {
+            currentShape.x--;
+        }
+           
+    }   
 }
 
 function leftMove() {
-    if ((currentShape.x - 1) >= 0) 
+    if ((currentShape.x - 1) >= 0 && !collision(currentShape.x - 1, currentShape.y, currentShape.shape))
         currentShape.x--;
 }
 
 function rightMove() {
     let width = currentShape.shape[0].length;
-    if ((currentShape.x + width) < 10)
+    if ((currentShape.x + width) < 10 && !collision((currentShape.x + 1), currentShape.y, currentShape.shape))
         currentShape.x++;
 }
 
@@ -158,36 +220,148 @@ function drawGrid() {
     }
 }
 
-let count = 0; // for tests
+function checkLose() {
+    if (currentShape.y == 0 && collision(currentShape.x, currentShape.y + 1, currentShape.shape)) {
+        clearInterval(interval);
+        lose.style.display = "block";
+    }
+}
+
+function clearWin(currentY) {
+    for (let x = 0; x < 10; ++x)
+        field[currentY][x] = 0;
+
+    for (let y = currentY - 1; y > 0; --y) {
+        for (let x = 0; x < 10; ++x) {
+            if (field[y][x]) {
+                field[y][x] = 0;
+                field[y + 1][x] = 1;
+            }
+        }
+    }
+}
+
+function checkWin() {
+    let match = 0;
+    for (let y = field.length - 1; y >= 0; --y) {
+        for (let x = 0; x < field[y].length; ++x) {
+            if (field[y][x]) ++match;
+        }
+        if (match == 10) {
+            score += 10;
+            scoreSpan.textContent = score;
+            clearWin(y);
+        }
+        match = 0;
+    }
+}
+
+function drawFake() {
+    let fakeShape = currentShape.shape;
+    let xFake = currentShape.x;
+    let yFake = currentShape.y;
+
+    while (!collision(xFake, yFake, fakeShape)) {
+        yFake++;
+        if (yFake > (20 - fakeShape.length)) {
+            break;
+        }
+    }
+
+    for (let y = 0; y < fakeShape.length; ++y) {
+        for (let x = 0; x < fakeShape[y].length; ++x) {
+            if (fakeShape[y][x]) {
+                ctx.fillStyle = "rgba(255,0,0,0.5)";
+                ctx.fillRect(
+                    (xFake + x) * BLOCK_SIZE,
+                    ((yFake - 1) + y) * BLOCK_SIZE,  
+                    BLOCK_SIZE - 1,             
+                    BLOCK_SIZE - 1  
+                );
+            }
+        }
+    }
+}
 
 function draw() {
-
+    if (pause) return;
+    
     drawGrid();
     currentShape.drawShape();
+    drawFake();
+    checkWin();
+    checkLose();
     currentShape.y++;
-
-    if (currentShape.y > (20 - currentShape.shape.length)) {
-            count++;
-
-            console.log(field);
+    
+    if (currentShape.y > (20 - currentShape.shape.length) || collision(currentShape.x, currentShape.y, currentShape.shape)) {
             currentShape.lockShape();
-            drawGrid();
-
-            // clearInterval(interval);
-
             currentShape = shapes[getRandomInt(7)];
             currentShape.y = 0;
             currentShape.x = 4;
+            defauleShapes();
+    }
+}
 
-            if (count == 2)
-                clearInterval(interval);
-            console.log(count);
+function buttonEvents(e) {
+    switch (e.key) {
+        case 's':
+            speedUp();
+            break;
+        case 'a':
+            leftMove();
+            break;
+        case 'd':
+            rightMove();
+            break;
+        case 'w':
+            rotationMove();
+            break;
     }
 }
 
 function gameLoop() {
+    defauleShapes();
+    addEvents();
+    start.removeEventListener("click", gameLoop);
+    restart.addEventListener("click", restartFunc);
     currentShape = shapes[getRandomInt(7)];
-    interval = setInterval(draw, 100);
+    interval = setInterval(draw, SPEED);
 }
 
-gameLoop();
+function stopfun() {
+    pause = !pause;
+    if (pause) {
+        stop.textContent = "continue";
+        removeEvents();
+    } else {
+        stop.textContent = "STOP";
+        addEvents();
+    }
+}
+
+start.addEventListener("click", gameLoop);
+
+function restartFunc() {
+    removeEvents();
+    restart.removeEventListener("click", restartFunc);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+
+    stop.textContent = "STOP";
+    field = [];
+    currentShape = null;
+    score = 0;
+    pause = false;
+    clearInterval(interval);
+    interval = null;
+    lose.style.display = "none";
+
+    for (let i = 0; i < 20; ++i) {
+        let row = [];
+        for (let j = 0; j < 10; ++j) {
+            row.push(0);
+        }
+        field.push(row);
+    }
+
+    gameLoop();
+}
